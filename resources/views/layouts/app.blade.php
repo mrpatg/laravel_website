bc945797ef5d9a3d190dc474823ba8f7ddbabbec
Patrick Godbey <mrpatg@gmail.com>
2018-07-01 18:25:55 -0400

    navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
 
     </head>
     <body>
-        @yield('content')
+        @include('inc.navbar')
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
