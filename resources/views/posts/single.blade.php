63cc13f42953e19fe0c6ee8fe918e3d3cfe735b1
Patrick Godbey <mrpatg@gmail.com>
2018-07-03 19:19:21 -0400

    fubared up the hidden params, fixed

--- resources/views/posts/single.blade.php
+++ resources/views/posts/single.blade.php
@@ -11,7 +11,7 @@
     <hr>
     <a href="/posts/{{$post->id}}/edit" class="btn btn-light">Edit</a>
     {!!Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
-        {{Form::hidden('_method' => 'DELETE')}}
+        {{Form::hidden('_method', 'DELETE')}}
         {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
     {!!Form::close() !!}
 @endsection
