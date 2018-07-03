6a511e7eb98d586bc76196711f71d7f749a200e8
Patrick Godbey <mrpatg@gmail.com>
2018-07-03 19:18:33 -0400

    delete button

--- resources/views/posts/single.blade.php
+++ resources/views/posts/single.blade.php
@@ -10,4 +10,8 @@
     <small>Written on {{$post->created_at}}</small>
     <hr>
     <a href="/posts/{{$post->id}}/edit" class="btn btn-light">Edit</a>
+    {!!Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+        {{Form::hidden('_method' => 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+    {!!Form::close() !!}
 @endsection
