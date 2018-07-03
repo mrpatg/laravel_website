ea59c7fb1bbab1e594cab3a487faabe9e3d95750
Patrick Godbey <mrpatg@gmail.com>
2018-07-03 18:48:41 -0400

    validation

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -53,7 +53,12 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required',
+
+        ]);
+        
     }
 
     /**
