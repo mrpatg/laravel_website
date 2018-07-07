463e077dac62d96f4c01be254bbd0a0e77ec44a7
Patrick Godbey <mrpatg@gmail.com>
2018-07-07 08:15:14 -0400

    bad classname

--- database/migrations/2018_07_07_120045_add_cover_image_to_posts.php
+++ database/migrations/2018_07_07_120045_add_cover_image_to_posts.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddUserIdToPosts extends Migration
+class AddCoverImageToPosts extends Migration
 {
     /**
      * Run the migrations.
