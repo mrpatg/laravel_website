e04b4162eafcd4415236cdfcdd982c6c827b5475
Patrick Godbey <mrpatg@gmail.com>
2018-07-07 07:57:44 -0400

    Paamayim Nekudotayim

--- resources/views/posts/single.blade.php
+++ resources/views/posts/single.blade.php
@@ -9,8 +9,8 @@
     <hr>
     <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
     <hr>
-    @if(!Auth:guest())
-        @if(Auth++user()->id == $post->user_id)
+    @if(!Auth::guest())
+        @if(Auth::user()->id == $post->user_id)
             <a href="/posts/{{$post->id}}/edit" class="btn btn-light">Edit</a>
             {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
                 {{Form::hidden('_method', 'DELETE')}}
